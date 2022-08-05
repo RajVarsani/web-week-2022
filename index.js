e90cb0120bb1271d47e816dddd71833b8c2587f2
Akash Gupta <akash.gupta.anup@gmail.com>
2022-08-05 19:36:33 +0530

    Added code

---
+++ index.js
@@ -0,0 +1,114 @@
+// console.log("hello hehe world");
+
+// Global and block - functional
+// let a = 1;
+// let b = "hello";
+
+// console.log(a, b);
+// const c = 4;
+// c = 5;
+
+/*
+hello
+*/
+// Javascript is case sensitive
+
+// let a = 5;
+// let b = 6;
+
+// console.log(a + b, a - b, a * b, a / b, a % b, a++, a--);
+
+// Data types
+// let a = 5;
+// let b = 5.5;
+// let c = "Akash";
+// let d = true;
+// let e;
+
+// console.log(typeof e);
+
+// let arr = [1, "hello", true, 4, 5];
+// console.log(arr);
+
+// let obj = {
+//   firstName: "Akash",
+//   lastName: "Gupta",
+//   age: 20,
+//   college: "IIIT Gwalior",
+// };
+// console.log(obj.firstName);
+
+// function
+// function sayHello(param) {
+//   console.log("hello" + " " + param);
+// }
+// sayHello("Akash");
+
+// // Arrow function
+// const sayHello2 = (fsfsfs) => {
+//     fsfksljfksjfklsjfksjfsfsf
+// };
+// sayHello2();
+
+// let arr = new Array(1, 2, 3, 4, 5);
+// arr[0] = 10;
+// arr.push(6);
+// arr.pop();
+// arr.sort();
+// arr.find();
+// console.log(arr);
+
+// const num = 5;
+// if (num < 5) {
+//   console.log("number less than 5");
+// } else if (num == 5) {
+//   console.log("Number is 5");
+// } else {
+//   console.log("number greater than 5");
+// }
+
+// let arr = new Array(1, 2, 3, 4, 5);
+// let sum = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//   sum = sum + arr[i];
+// }
+// console.log(sum);
+
+// Map
+// const newArray = arr.map((item, index) => {
+//   return item * index;
+// });
+// console.log(newArray);
+
+// Json  - javascript object notation
+
+// let obj = {
+//   firstName: "Akash",
+//   lastName: "Gupta",
+//   age: 20,
+//   college: "IIIT Gwalior",
+// };
+
+// const json = JSON.stringify(obj);
+// const jsonToObj = JSON.parse(json);
+// console.log(json, jsonToObj);
+
+// Dates
+
+// let date = new Date().getMilliseconds();
+// console.log(date);
+
+// setTimeout(() => {
+//   console.log("heelo");
+// }, 2000);
+
+// setInterval(() => {
+//   console.log(
+//     new Date().getHours() +
+//       "-" +
+//       new Date().getMinutes() +
+//       "-" +
+//       new Date().getSeconds()
+//   );
+// }, 2000);
